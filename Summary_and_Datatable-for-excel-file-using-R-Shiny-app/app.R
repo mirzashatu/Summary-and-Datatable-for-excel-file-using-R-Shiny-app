f6185f43485de1ed06c0283b643b8fdbefc25511
mirzashatu <84579979+mirzashatu@users.noreply.github.com>
2025-05-11 19:34:47 +0600

    Update app.R

--- Summary_and_Datatable-for-excel-file-using-R-Shiny-app/app.R
+++ Summary_and_Datatable-for-excel-file-using-R-Shiny-app/app.R
@@ -1,51 +1,114 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    https://shiny.posit.co/
-#
-
 library(shiny)
+library(readxl)
+library(DT)
 
-# Define UI for application that draws a histogram
+# List of built-in datasets
+built_in_datasets <- c("mtcars", "iris", "faithful")
+
+
 ui <- fluidPage(
-
-    # Application title
-    titlePanel("Old Faithful Geyser Data"),
-
-    # Sidebar with a slider input for number of bins 
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
-        ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-           plotOutput("distPlot")
-        )
+  titlePanel("student survey data dashboard"),
+  
+  sidebarLayout(
+    sidebarPanel(
+      #dataset buildin data
+      radioButtons("data_source","choose data source:",
+                   choices = c("Built-in Dataset","Upload excel file"),
+                   selected = "Built-in Dataset"
+      ),
+      #Built in dataset selection
+      conditionalPanel(
+        condition = "input.data_source == 'Built-in Dataset'",
+        selectInput("built-in","Select built in data set",choices = built_in_datasets )
+      ),
+      conditionalPanel(
+        condition = "input.data_source == 'Upload excel file'",
+        fileInput("file","Upload excel File",accept = c("xlsx")),
+        uiOutput("sheet_ui")
+      ),
+      
+      
+      
+      actionButton("submit", "Load Data")
+    ),
+    #tab layout
+    tabsetPanel(
+      tabPanel("summary",
+               verbatimTextOutput("dataSummary")),
+      tabPanel("Data Table",
+               DTOutput("dataTable")
+      )
     )
+  )
+  
+  
+  
+  
 )
 
-# Define server logic required to draw a histogram
-server <- function(input, output) {
-
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white',
-             xlab = 'Waiting time to next eruption (in mins)',
-             main = 'Histogram of waiting times')
-    })
+server <- function(input, output, session) {
+  #reactive expression  to store  uploaded file path
+  file_path <- reactive({
+    req(input$file)
+    input$file$datapath
+  })
+  
+  #observe the uploaded file and update the sheet
+  observeEvent(input$file,{
+    sheets <- excel_sheets(file_path()) 
+    updateSelectInput(session,
+                      "sheet",
+                      "Select sheet",
+                      choices = sheets,
+                      selected = sheets[1])
+  })
+  
+  #Generate UI for sheet selection 
+  output$sheet_ui <- renderUI(
+    {
+      req(input$file)
+      selectInput("sheet","Select Sheet",choices=NULL)
+    }
+  )
+  # Reactive function to load data based on user selection
+  selected_data <- eventReactive(input$submit,{
+    if(input$data_source == "Built-in Dataset"){
+      get(input$'built-in')
+    } else{
+      req(input$file,input$sheet)
+      read_excel(file_path(),sheet = input$sheet)
+    }
+    
+  })
+  
+  #Render dataset summary
+  output$dataSummary <- renderPrint({
+    req(selected_data())
+    summary(selected_data())
+  })
+  
+  
+  #Render data as a table
+  output$dataTable <- renderDT({
+    req(selected_data())
+    datatable(selected_data())
+  })
+  
+  
+  
 }
 
-# Run the application 
-shinyApp(ui = ui, server = server)
+shinyApp(ui, server)
+
+
+
+
+
+
+
+
+
+
+
+
+
